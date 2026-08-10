380f577f5326c61a3ed367513bae74292e60bd20
amolvkharche <83961171+amolvkharche@users.noreply.github.com>
2026-08-10 11:49:02 +0530

    Update AWS provider to use variables for keys

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = ""
-  secret_key = ""
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = {
       ClusterName = var.cluster_name
